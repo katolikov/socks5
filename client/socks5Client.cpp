c7ac865a63b7bad5592b8ab6555c64d6d8cedae1
Katolikov <katolikovart@gmail.com>
2020-06-11 01:52:07 +0300

    update

--- client/socks5Client.cpp
+++ client/socks5Client.cpp
@@ -113,33 +113,36 @@ int main(int argc, char *argv[]) {
             boost::asio::ip::tcp::endpoint ep{boost::asio::ip::tcp::v4(), port};
 
             //acceptor.open(ep.protocol());
-
             boost::asio::ip::tcp::socket socket{make_strand(acceptor.get_executor())};
             boost::system::error_code ec;
+            boost::system::error_code error_socket;
+            socket.async_connect(ep, yc[error_socket]);
 
-            socket.async_connect(ep, yc[ec]);
-
-            constexpr static std::chrono::seconds timeout{20};
-
-            boost::asio::io_context io_context;
-            boost::asio::ip::tcp::resolver resolver(io_context);
-
-            auto http_endpoint = *resolver.resolve(
-                    boost::asio::ip::tcp::v4(), "ya.ru", "https");
-
-            if (ec == boost::asio::error::operation_aborted)
+            if (error_socket == boost::asio::error::operation_aborted) {
                 return;
-            if (ec)
-                logger{} << "Failed to connect: " << ec.message();
-            else {
+            }
+            if (error_socket) {
+                logger{} << "Failed to connect: " << error_socket.message();
+            } else {
+
+                constexpr static std::chrono::seconds timeout{20};
+
+                boost::asio::io_context io_context;
+                boost::asio::ip::tcp::resolver resolver(io_context);
+
+                auto http_endpoint = *resolver.resolve(
+                        boost::asio::ip::tcp::v4(), "government.ru", "http");
+
                 for (;;) {
 
+                    socket.async_connect(ep, yc[error_socket]);
 
                     socks5::request_first socks_request_first;
 
-                    //stream.expires_after(timeout);
+                    //socket.expires_after(timeout);
 
-                    async_write(socket, socks_request_first.buffers(), yc[ec]);
+                    socket.async_write_some(socks_request_first.buffers(), yc[ec]);
+
                     if (ec) {
                         if (ec != boost::asio::error::operation_aborted)
                             logger{} << "Failed to write first request: "
@@ -161,7 +164,7 @@ int main(int argc, char *argv[]) {
                     socks5::request_second socks_request_second(
                             socks5::request_second::connect, http_endpoint);
 
-                    async_write(socket, socks_request_second.buffers(), yc[ec]);
+                    socket.async_write_some(socks_request_second.buffers(), yc[ec]);
                     if (ec) {
                         if (ec != boost::asio::error::operation_aborted)
                             logger{} << "Failed to write second request: "
@@ -179,109 +182,51 @@ int main(int argc, char *argv[]) {
                         return;
                     }
 
-                    std::cout << "1\n";
+                    std::string request =
+                            "GET / HTTP/1.0\r\n"
+                            "Host: www.government.ru\r\n"
+                            "Accept: */*\r\n"
+                            "Connection: close\r\n\r\n";
 
-                    /*if (ec == boost::asio::error::operation_aborted)
+                    // Send the HTTP request.
+                    //stream.expires_after(timeout);
+                    socket.async_write_some(boost::asio::buffer(request), yc[ec]);
+
+                    // Read until EOF, writing data to output as we go.
+                    std::array<char, 512> response{};
+                    boost::system::error_code error;
+
+                    while (std::size_t s = socket.async_read_some(
+                            boost::asio::buffer(response), yc[ec]))
+                        std::cout.write(response.data(), s);
+
+                    if (ec) {
+                        if (ec != boost::asio::error::operation_aborted)
+                            logger{} << "Close Error: " << ec.message();
                         return;
-                    if (ec)
-                        logger{} << "Failed to connect: " << ec.message();
-                    else*/
+                    }
 
-                    bccoro::spawn(bind_executor(socket.get_executor(),
-                                                [stream = boost::beast::tcp_stream{std::move(socket)}]
-                                                        (bccoro::yield_context yc) mutable {
+                    //socket.async_connect(ep, yc[ec]);
 
-                                                    //constexpr static std::chrono::seconds timeout{20};
-                                                    //constexpr static std::size_t limit = 1024;
-                                                    boost::system::error_code ec;
-                                                    std::cout << "2\n";
-                                                    //std::string in_buf, out_buf;
-                                                    //bigint a;
+                    /*bccoro::spawn(bind_executor(socket.get_executor(),
+                                                     [stream = boost::beast::tcp_stream {std::move(socket)}]
+                                                             (bccoro::yield_context yc) mutable {
 
-                                                    /*auto read_buffered_number = [&](std::size_t n) {
-                                                        bigint x{in_buf.substr(0, n - 1)};
-                                                        in_buf.erase(0, n);
-                                                        return x;
-                                                    };*/
+                                                         boost::system::error_code ec;
 
-                                                    for (;;) {
+                                                         boost::asio::io_context io_context;
+                                                         boost::asio::ip::tcp::resolver resolver(io_context);
 
-                                                        std::string request =
-                                                                "GET / HTTPS/1.0\r\n"
-                                                                "Host: www.ya.ru\r\n"
-                                                                "Accept: */*\r\n"
-                                                                "Connection: close\r\n\r\n";
+                                                         auto http_endpoint = *resolver.resolve(
+                                                                 boost::asio::ip::tcp::v4(), "government.ru", "http");
 
-                                                        // Send the HTTP request.
-                                                        async_write(stream, boost::asio::buffer(request), yc[ec]);
+                                                         for (;;) {
 
-                                                        // Read until EOF, writing data to output as we go.
-                                                        std::array<char, 512> response{};
-                                                        boost::system::error_code error;
-
-                                                        while (std::size_t s = stream.async_read_some(
-                                                                boost::asio::buffer(response), yc[ec]))
-                                                            std::cout.write(response.data(), s);
-
-                                                        if (ec) {
-                                                            if (ec != boost::asio::error::operation_aborted)
-                                                                logger{} << "Close Error: " << ec.message();
-                                                            return;
-                                                        }
-
-                                                        std::cout << "3\n";
-
-                                                        /*
-                                                        std::size_t n = async_read_until(
-                                                                stream,
-                                                                boost::asio::dynamic_string_buffer(in_buf, limit),
-                                                                '\n', yc[ec]);
-                                                        if (ec) {
-                                                            if (ec != boost::asio::error::operation_aborted &&
-                                                                (ec != boost::asio::error::eof || n))
-                                                                logger{} << "Failed to read a: " << ec.message();
-                                                            return;
-                                                        }
-                                                        try {
-                                                            a = read_buffered_number(n);
-                                                        }
-                                                        catch (...) {
-                                                            logger{}
-                                                                    << boost::current_exception_diagnostic_information();
-                                                            return;
-                                                        }
-                                                        stream.expires_after(timeout);
-                                                        n = async_read_until(
-                                                                stream,
-                                                                boost::asio::dynamic_string_buffer(in_buf, limit),
-                                                                '\n', yc[ec]);
-                                                        if (ec) {
-                                                            if (ec != boost::asio::error::operation_aborted &&
-                                                                (ec != boost::asio::error::eof || n))
-                                                                logger{} << "Failed to read b: " << ec.message();
-                                                            return;
-                                                        }
-                                                        try {
-                                                            out_buf = bigint{a * read_buffered_number(n)}.str() + '\n';
-                                                        }
-                                                        catch (...) {
-                                                            logger{}
-                                                                    << boost::current_exception_diagnostic_information();
-                                                            return;
-                                                        }
-
-                                                        stream.expires_after(timeout);
-                                                        async_write(stream, boost::asio::buffer(out_buf), yc[ec]);
-                                                        if (ec) {
-                                                            if (ec != boost::asio::error::operation_aborted)
-                                                                logger{} << "Failed to write result: " << ec.message();
-                                                            return;
-                                                        }
-                                                        */
-                                                    }
-                                                }));
+                                                         }
+                                                     }));*/
                 }
             }
+
         }));
         std::vector<std::thread> workers;
         size_t extra_workers = std::thread::hardware_concurrency() - 1;
