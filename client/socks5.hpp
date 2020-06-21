50c2b83939b789acce14b0e699ebd916907f31fd
katolikov <katolikovart@gmail.com>
2020-06-22 01:33:12 +0300

    update + include count of client to connect

--- client/socks5.hpp
+++ client/socks5.hpp
@@ -8,179 +8,180 @@
 
 namespace socks5 {
 
-    const unsigned char version = 0x05;
+    namespace client {
+        const unsigned char version = 0x05;
 
-    class request_first {
-    public:
-        const unsigned char num = 0x01;
-        const unsigned char type = 0x00;
+        class request_first {
+        public:
+            const unsigned char num = 0x01;
+            const unsigned char type = 0x00;
 
-        request_first() : version_(version),
-                          auth_num_(num),
-                          auth_(type) {}
+            request_first() : version_(version),
+                              auth_num_(num),
+                              auth_(type) {}
 
 
-        std::array<boost::asio::const_buffer, 3> buffers() const {
-            return
-                    {
-                            {
-                                    boost::asio::buffer(&version_, 1),
-                                    boost::asio::buffer(&auth_num_, 1),
-                                    boost::asio::buffer(&auth_, 1)
-                            }
-                    };
-        }
-
-    private:
-        unsigned char version_;
-        unsigned char auth_num_;
-        unsigned char auth_;
-    };
-
-    class request_second {
-    public:
-        const unsigned char addr_type = 0x01;
-
-        enum command_type {
-            connect = 0x01,
-        };
-
-        request_second(command_type cmd, const boost::asio::ip::tcp::endpoint &endpoint)
-                : version_(version),
-                  command_(cmd),
-                  null_byte_(0),
-                  type_address_(addr_type) {
-            if (endpoint.protocol() != boost::asio::ip::tcp::v4()) {
-                throw boost::system::system_error(
-                        boost::asio::error::address_family_not_supported);
+            std::array<boost::asio::const_buffer, 3> buffers() const {
+                return
+                        {
+                                {
+                                        boost::asio::buffer(&version_, 1),
+                                        boost::asio::buffer(&auth_num_, 1),
+                                        boost::asio::buffer(&auth_, 1)
+                                }
+                        };
             }
 
-            if (endpoint.protocol() != boost::asio::ip::tcp::v4()) {
-                throw boost::system::system_error(
-                        boost::asio::error::address_family_not_supported);
+        private:
+            unsigned char version_;
+            unsigned char auth_num_;
+            unsigned char auth_;
+        };
+
+        class request_second {
+        public:
+            const unsigned char addr_type = 0x01;
+
+            enum command_type {
+                connect = 0x01,
+            };
+
+            request_second(command_type cmd, const boost::asio::ip::tcp::endpoint &endpoint)
+                    : version_(version),
+                      command_(cmd),
+                      null_byte_(0),
+                      type_address_(addr_type) {
+                if (endpoint.protocol() != boost::asio::ip::tcp::v4()) {
+                    throw boost::system::system_error(
+                            boost::asio::error::address_family_not_supported);
+                }
+
+                if (endpoint.protocol() != boost::asio::ip::tcp::v4()) {
+                    throw boost::system::system_error(
+                            boost::asio::error::address_family_not_supported);
+                }
+
+                unsigned short port = endpoint.port();
+                port_high_byte_ = (port >> 8) & 0xff;
+                port_low_byte_ = port & 0xff;
+
+                address_ = endpoint.address().to_v4().to_bytes();
             }
 
-            unsigned short port = endpoint.port();
-            port_high_byte_ = (port >> 8) & 0xff;
-            port_low_byte_ = port & 0xff;
+            std::array<boost::asio::const_buffer, 10> buffers() const {
+                return
+                        {
+                                {
+                                        boost::asio::buffer(&version_, 1),
+                                        boost::asio::buffer(&command_, 1),
+                                        boost::asio::buffer(&null_byte_, 1),
+                                        boost::asio::buffer(&type_address_, 1),
+                                        boost::asio::buffer(address_, 4),
+                                        boost::asio::buffer(&port_high_byte_, 1),
+                                        boost::asio::buffer(&port_low_byte_, 1),
+                                }
+                        };
+            }
 
-            address_ = endpoint.address().to_v4().to_bytes();
-        }
-
-        std::array<boost::asio::const_buffer, 10> buffers() const {
-            return
-                    {
-                            {
-                                    boost::asio::buffer(&version_, 1),
-                                    boost::asio::buffer(&command_, 1),
-                                    boost::asio::buffer(&null_byte_, 1),
-                                    boost::asio::buffer(&type_address_, 1),
-                                    boost::asio::buffer(address_, 4),
-                                    boost::asio::buffer(&port_high_byte_, 1),
-                                    boost::asio::buffer(&port_low_byte_, 1),
-                            }
-                    };
-        }
-
-    private:
-        unsigned char version_;
-        unsigned char command_;
-        unsigned char null_byte_;
-        unsigned char type_address_;
-        boost::asio::ip::address_v4::bytes_type address_;
-        unsigned char port_high_byte_;
-        unsigned char port_low_byte_;
-    };
-
-    class reply_first {
-    public:
-        enum auth_ {
-            no_auth = 0x00,
+        private:
+            unsigned char version_;
+            unsigned char command_;
+            unsigned char null_byte_;
+            unsigned char type_address_;
+            boost::asio::ip::address_v4::bytes_type address_;
+            unsigned char port_high_byte_;
+            unsigned char port_low_byte_;
         };
 
-        reply_first()
-                : version_(),
-                  status_() {
-        }
+        class reply_first {
+        public:
+            enum auth_ {
+                no_auth = 0x00,
+            };
 
-        std::array<boost::asio::mutable_buffer, 2> buffers() {
-            return
-                    {
-                            {
-                                    boost::asio::buffer(&version_, 1),
-                                    boost::asio::buffer(&status_, 1),
-                            }
-                    };
-        }
+            reply_first()
+                    : version_(),
+                      status_() {
+            }
 
-        bool success() const {
-            return status_ == no_auth;
-        }
+            std::array<boost::asio::mutable_buffer, 2> buffers() {
+                return
+                        {
+                                {
+                                        boost::asio::buffer(&version_, 1),
+                                        boost::asio::buffer(&status_, 1),
+                                }
+                        };
+            }
 
-        unsigned char status() const {
-            return status_;
-        }
+            bool success() const {
+                return status_ == no_auth;
+            }
 
-    private:
-        unsigned char version_;
-        unsigned char status_;
-    };
+            unsigned char status() const {
+                return status_;
+            }
 
-    class reply_second {
-    public:
-        enum status_type {
-            request_granted = 0x00,
-            request_failed = 0x01,
+        private:
+            unsigned char version_;
+            unsigned char status_;
         };
 
-        reply_second()
-                : version_(),
-                  status_(),
-                  null_byte_(0),
-                  type_addr_() {
-        }
+        class reply_second {
+        public:
+            enum status_type {
+                request_granted = 0x00,
+                request_failed = 0x01,
+            };
 
-        std::array<boost::asio::mutable_buffer, 10> buffers() {
-            return
-                    {
-                            {
-                                    boost::asio::buffer(&version_, 1),
-                                    boost::asio::buffer(&status_, 1),
-                                    boost::asio::buffer(&null_byte_, 1),
-                                    boost::asio::buffer(&type_addr_, 1),
-                                    boost::asio::buffer(address_, 4),
-                                    boost::asio::buffer(&port_high_byte_, 2),
+            reply_second()
+                    : version_(),
+                      status_(),
+                      null_byte_(0),
+                      type_addr_() {
+            }
 
-                            }
-                    };
-        }
+            std::array<boost::asio::mutable_buffer, 10> buffers() {
+                return
+                        {
+                                {
+                                        boost::asio::buffer(&version_, 1),
+                                        boost::asio::buffer(&status_, 1),
+                                        boost::asio::buffer(&null_byte_, 1),
+                                        boost::asio::buffer(&type_addr_, 1),
+                                        boost::asio::buffer(address_, 4),
+                                        boost::asio::buffer(&port_high_byte_, 2),
 
-        bool success() const {
-            return null_byte_ == 0 && status_ == request_granted;
-        }
+                                }
+                        };
+            }
 
-        unsigned char status() const {
-            return status_;
-        }
+            bool success() const {
+                return null_byte_ == 0 && status_ == request_granted;
+            }
 
-        boost::asio::ip::tcp::endpoint endpoint() const {
-            unsigned short port = port_high_byte_;
-            port = (port << 8) & 0xff00;
+            unsigned char status() const {
+                return status_;
+            }
 
-            boost::asio::ip::address_v4 address(address_);
+            boost::asio::ip::tcp::endpoint endpoint() const {
+                unsigned short port = port_high_byte_;
+                port = (port << 8) & 0xff00;
 
-            return boost::asio::ip::tcp::endpoint(address, port);
-        }
+                boost::asio::ip::address_v4 address(address_);
 
-    private:
-        unsigned char version_;
-        unsigned char status_;
-        unsigned char null_byte_;
-        unsigned char type_addr_;
-        boost::asio::ip::address_v4::bytes_type address_;
-        unsigned char port_high_byte_;
-    };
+                return boost::asio::ip::tcp::endpoint(address, port);
+            }
 
+        private:
+            unsigned char version_;
+            unsigned char status_;
+            unsigned char null_byte_;
+            unsigned char type_addr_;
+            boost::asio::ip::address_v4::bytes_type address_;
+            unsigned char port_high_byte_;
+        };
+    }
 } // namespace socks5
 
 #endif // SOCKS5_HPP
--- client/socks5Client.cpp
+++ client/socks5Client.cpp
@@ -16,6 +16,7 @@
 #include <charconv>
 #include <cstdint>
 #include <iostream>
+#include <vector>
 #include <memory>
 #include <mutex>
 #include <optional>
@@ -92,11 +93,14 @@ auto at_scope_exit(F &&f) {
 int main(int argc, char *argv[]) {
 
     try {
-        if (argc != 2)
+        if (argc < 2)
             throw std::runtime_error(format("Usage: ", argv[0], " <listen-port>"));
         auto port = from_chars<std::uint16_t>(argv[1]);
         if (!port || !*port)
             throw std::runtime_error("Port must be in [1;65535]");
+        auto num = from_chars<int>(argv[2]);
+        if (!num)
+            throw std::runtime_error("Number of client must be large then 0");
         boost::asio::io_context ctx;
         boost::asio::signal_set stop_signals{ctx, SIGINT, SIGTERM};
         stop_signals.async_wait([&](boost::system::error_code ec, int /*signal*/) {
@@ -106,93 +110,100 @@ int main(int argc, char *argv[]) {
             ctx.stop();
         });
 
-        bccoro::spawn(bind_executor(ctx, [&ctx, port = *port](bccoro::yield_context yc) {
+        for (auto i = 0; i < num; ++i) {
 
-            boost::asio::ip::tcp::endpoint ep{boost::asio::ip::tcp::v4(), port};
-            constexpr static std::chrono::seconds timeout{20};
+            boost::asio::ip::tcp::socket socket_new{make_strand(ctx)};
+
             boost::asio::ip::tcp::resolver resolver(ctx);
+
             auto http_endpoint = *resolver.resolve(
                     boost::asio::ip::tcp::v4(), "www.example.com", "http");
 
-            for (;;) {
+            bccoro::spawn(bind_executor(ctx,
+                                        [socket = boost::beast::tcp_stream{
+                                                std::move(socket_new)}, http_endpoint, port = *port]
+                                                (bccoro::yield_context yc) mutable {
 
-                boost::system::error_code ec;
-                boost::beast::tcp_stream socket{make_strand(ctx)};
-                socket.async_connect(ep, yc[ec]);
+                                            boost::asio::ip::tcp::endpoint ep{boost::asio::ip::tcp::v4(), port};
+                                            constexpr static std::chrono::seconds timeout{20};
+                                            boost::system::error_code ec;
 
-                if (ec == boost::asio::error::operation_aborted) {
-                    return;
-                }
+                                            for (;;) {
 
-                if (ec) {
-                    logger{} << "Failed to connect: " << ec.message();
-                }
+                                                socket.async_connect(ep, yc[ec]);
 
-                socks5::request_first socks_request_first;
-                socket.expires_after(timeout);
-                socket.async_write_some(socks_request_first.buffers(), yc[ec]);
+                                                if (ec == boost::asio::error::operation_aborted) {
+                                                    return;
+                                                }
 
-                if (ec) {
-                    logger{} << "Failed to write first request: "
-                             << ec.message();
-                }
+                                                if (ec) {
+                                                    logger{} << "Failed to connect: " << ec.message();
+                                                }
 
-                socket.expires_after(timeout);
-                socks5::reply_first socks_reply_first;
-                socket.async_read_some(socks_reply_first.buffers(), yc[ec]);
+                                                socks5::client::request_first socks_request_first;
+                                                socket.expires_after(timeout);
+                                                socket.async_write_some(socks_request_first.buffers(), yc[ec]);
 
-                if (ec) {
-                    logger{} << "First reply error: " << ec.message();
-                }
+                                                if (ec) {
+                                                    logger{} << "Failed to write first request: "
+                                                             << ec.message();
+                                                }
 
-                socket.expires_after(timeout);
-                socks5::request_second socks_request_second(
-                        socks5::request_second::connect, http_endpoint);
-                socket.async_write_some(socks_request_second.buffers(), yc[ec]);
+                                                socket.expires_after(timeout);
+                                                socks5::client::reply_first socks_reply_first;
+                                                socket.async_read_some(socks_reply_first.buffers(), yc[ec]);
 
-                if (ec) {
-                    logger{} << "Failed to write second request: "
-                             << ec.message();
-                }
+                                                if (ec) {
+                                                    logger{} << "First reply error: " << ec.message();
+                                                }
 
-                socket.expires_after(timeout);
-                socks5::reply_second socks_reply_second;
-                socket.async_read_some(socks_reply_second.buffers(), yc[ec]);
+                                                socket.expires_after(timeout);
+                                                socks5::client::request_second socks_request_second(
+                                                        socks5::client::request_second::connect, http_endpoint);
+                                                socket.async_write_some(socks_request_second.buffers(), yc[ec]);
 
-                if (ec) {
-                    logger{} << "Second reply error: " << ec.message();
-                }
+                                                if (ec) {
+                                                    logger{} << "Failed to write second request: "
+                                                             << ec.message();
+                                                }
 
-                std::string request =
-                        "GET / HTTP/1.0\r\n"
-                        "Host: www.example.com\r\n"
-                        "Accept: */*\r\n"
-                        "Connection: close\r\n\r\n";
+                                                socket.expires_after(timeout);
+                                                socks5::client::reply_second socks_reply_second;
+                                                socket.async_read_some(socks_reply_second.buffers(), yc[ec]);
 
-                // Send the HTTP request.
-                socket.expires_after(timeout);
-                socket.async_write_some(boost::asio::buffer(request), yc[ec]);
+                                                if (ec) {
+                                                    logger{} << "Second reply error: " << ec.message();
+                                                }
 
-                // Read until EOF, writing data to output as we go.
-                std::array<char, 512> response{};
-                boost::system::error_code error;
-                while (std::size_t s = socket.async_read_some(
-                        boost::asio::buffer(response), yc[ec]))
-                    std::cout.write(response.data(), s);
-                if (ec) {
-                    logger{} << "Close Error: " << ec.message();
-                }
-            }
+                                                std::string request =
+                                                        "GET / HTTP/1.0\r\n"
+                                                        "Host: www.example.com\r\n"
+                                                        "Accept: */*\r\n"
+                                                        "Connection: close\r\n\r\n";
 
-        }));
+                                                // Send the HTTP request.
+                                                socket.expires_after(timeout);
+                                                socket.async_write_some(boost::asio::buffer(request), yc[ec]);
+
+                                                // Read until EOF, writing data to output as we go.
+                                                std::array<char, 512> response{};
+                                                while (std::size_t s = socket.async_read_some(
+                                                        boost::asio::buffer(response), yc[ec]))
+                                                    std::cout.write(response.data(), s);
+                                                if (ec) {
+                                                    logger{} << "Close Error: " << ec.message();
+                                                }
+                                            }
+                                        }));
+        }
         std::vector<std::thread> workers;
-        size_t extra_workers = std::thread::hardware_concurrency() - 1;
+        std::size_t extra_workers = std::thread::hardware_concurrency() - 1;
         workers.reserve(extra_workers);
         auto ase = at_scope_exit([&]() noexcept {
             for (auto &t:workers)
                 t.join();
         });
-        for (size_t i = 0; i < extra_workers; ++i)
+        for (std::size_t i = 0; i < extra_workers; ++i)
             workers.emplace_back([&] {
                 ctx.run();
             });
